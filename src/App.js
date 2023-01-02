93a0031ee77b153ab000fce88c15093846afbc59
Jaehee <brillar2225@gmail.com>
2023-01-02 23:07:04 +0900

    ADD: Fetch API url of movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title + 'image'} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((li, id) => (
+                  <li key={id}>{li}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
