5d2520d1660160ac5b38015a28f1690b72ffd578
Jaehee <brillar2225@gmail.com>
2023-01-04 20:06:06 +0900

    ADD: Add details of movie

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,45 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+
+  const getMovie = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(json.data.movie);
+  }, [id]);
+
+  useEffect(() => {
+    getMovie();
+  }, [getMovie]);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.large_cover_image} alt={`${movie.title} cover`} />
+          <h1>{movie.title}</h1>
+          <h3>{movie.description_full}</h3>
+          <p>Release Year : {movie.year}</p>
+          <p>Language : {movie.language}</p>
+          <p>Rating : {movie.rating}</p>
+          <ul>
+            {movie.genres.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
